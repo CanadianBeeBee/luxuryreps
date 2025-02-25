5aba2764d486fe70a35ba574aaed5427c31bf990
xreps <nathdsc919@gmail.com>
2025-02-25 21:18:10 +0100

    25.02.25 | 8

--- lib/cloudinary.ts
+++ lib/cloudinary.ts
@@ -4,6 +4,7 @@ cloudinary.config({
   cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
   api_key: "646748199176175",
   api_secret: "jExEbmIQ45BIrDByd0DKYMEdGiE",
+  secure : true 
 })
 
 export { cloudinary }
